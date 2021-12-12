4328eafe9b10dedb34b3b3b7b3659d5c751524ee
Hyun Yi <hy00un__@naver.com>
2021-12-12 17:01:31 +0900

    Update Float.fs

--- StudentScoresUsingModule/Float.fs
+++ StudentScoresUsingModule/Float.fs
@@ -5,10 +5,13 @@ module Float =
     let tryFromString (s : string) : float option = 
         if s = "N/A" then
             None
+            // Nothing
         else
             Some (float s)
+            // Something (float s)
 
     let fromStringOrNum (num : float, s : string) : float = 
         s
         |> tryFromString
         |> Option.defaultValue num
+        // Optional.defaultValue num
